04c3000823e50e0f4a8821eba6fe5fb88b3bb05e
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2021-04-16 12:53:14 +0200

    Update test script for old Debian compat

--- test/test.sh
+++ test/test.sh
@@ -5,7 +5,7 @@ for i in $(ls -d test/repos/*); do
     echo "deb file://${repo_path} ./" > /etc/apt/sources.list.d/datadog.list
 
     # if apt update passes, we correctly recognized repodata signature
-    apt update
+    apt-get update
 
     # verify package-level signature
     debsig-verify ${repo_path}/datadog-signing-keys*.deb
