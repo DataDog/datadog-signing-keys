8ce601438194bf55f978e1a14a942ca6dafa5330
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2021-04-23 11:07:32 +0200

    Fix tests

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -ex
 
-keyring="/usr/share/keyrings/datadog-archive-keyring.gpg"
+apt_trusted_keyring="/etc/apt/trusted.gpg.d/datadog-archive-keyring.gpg"
+usr_share_keyring="/usr/share/keyrings/datadog-archive-keyring.gpg"
 
 for i in $(ls -d test/repos/*); do
     repo_path=$(pwd)/${i}
-    echo "deb [signed-by=${keyring}] file://${repo_path} ./" > /etc/apt/sources.list.d/datadog.list
+    echo "deb [signed-by=${usr_share_keyring}] file://${repo_path} ./" > /etc/apt/sources.list.d/datadog.list
 
     # if apt update passes, we correctly recognized repodata signature
     apt-get update
@@ -13,10 +14,12 @@ for i in $(ls -d test/repos/*); do
     debsig-verify ${repo_path}/datadog-signing-keys*.deb
 done
 
-if [ "${ENSURE_TRUSTED_GPG_D_KEYRING}" = "true" ] && [ ! -f ${keyring} ]; then
-    echo "${keyring} doesn't exist when it should"
+if [ "${ENSURE_TRUSTED_GPG_D_KEYRING}" = "true" ] && [ ! -f ${apt_trusted_keyring} ]; then
+    echo "${apt_trusted_keyring} doesn't exist when it should"
+    exit 1
 fi
 
-if [ "${ENSURE_TRUSTED_GPG_D_KEYRING}" != "true" ] && [ -f ${keyring} ]; then
-    echo "${keyring} exists when it shouldn't"
+if [ "${ENSURE_TRUSTED_GPG_D_KEYRING}" != "true" ] && [ -f ${apt_trusted_keyring} ]; then
+    echo "${apt_trusted_keyring} exists when it shouldn't"
+    exit 1
 fi
