9c88be5a1bb8ba1d80e6a271fd75a5c860cccd62
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2021-05-07 13:28:53 +0200

    Add/change files as necessary for opensourcing

--- omnibus.rb
+++ omnibus.rb
@@ -1,3 +1,7 @@
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2021-present Datadog, Inc.
 #
 # This file is used to configure the datadog-signing-keys project. It contains
 # some minimal configuration examples for working with Omnibus. For a full list
