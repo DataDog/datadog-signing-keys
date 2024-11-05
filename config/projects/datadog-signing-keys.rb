0a68f776b6a986106017f079290cfd54233f805a
AliDatadog <ali.benabdallah@datadoghq.com>
2024-11-05 14:53:35 +0100

    Restructure the gitlab CI

--- config/projects/datadog-signing-keys.rb
+++ config/projects/datadog-signing-keys.rb
@@ -44,12 +44,6 @@ package :deb do
   section 'utils'
   priority 'extra'
   safe_architecture 'all'
-  if ENV.has_key?('DEB_SIGNING_PASSPHRASE') and not ENV['DEB_SIGNING_PASSPHRASE'].empty?
-    signing_passphrase "#{ENV['DEB_SIGNING_PASSPHRASE']}"
-    if ENV.has_key?('DEB_GPG_KEY_NAME') and not ENV['DEB_GPG_KEY_NAME'].empty?
-      gpg_key_name "#{ENV['DEB_GPG_KEY_NAME']}"
-    end
-  end
 end
 
 keys.each do |key|
